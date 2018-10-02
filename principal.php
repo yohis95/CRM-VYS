27aac691dbe292ae945a8f7c0d262697679a98d5
valeria7775 <42848371+valeria7775@users.noreply.github.com>
2018-10-02 20:48:25 -0300

    Add files via upload

---
+++ principal.php
@@ -0,0 +1,25 @@
+<?php
+require('objetos/generales/inicializacion.php');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HOLA</title>
+	<style type="text/css">
+		body{
+			background: url(images/fondo.jpg);
+		}
+	</style>
+</head>
+<body>
+
+	<header>
+
+		<?php
+		$direccion_imagen="images/logo.png";
+		 require('objetos/especificos/menu.php') ?>
+	</header>
+
+</body>
+</html>
