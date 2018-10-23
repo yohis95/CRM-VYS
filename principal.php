38aabfed6e26101da685d0a619a893757c5ee226
yohis95 <42844131+yohis95@users.noreply.github.com>
2018-10-23 11:37:01 -0300

    Add files via upload

---
+++ principal.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+require_once('objetos/generales/conexion.php'); 
+$link_error="index.php?error_usuario=si";
+require_once('objetos/generales/validar.php'); 
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CRM VYS</title>
+	<style type="text/css">
+		body{
+			background: url(images/fondo.jpg);
+		}
+	</style>
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
+
+	<header>
+		<nav class="navbar navbar-expand-lg navbar-dark " style="background: white; border-bottom: 1px solid #D01262">
+			<a href="#" class="navbar-brand"><img src="images/logo.png" width="50"></a>
+			<button class="navbar-toggler" data-target="#navigation" data-control="navigation" data-toggle="collapse">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navigation">
+				<ul class="nav navbar-nav">
+
+					<li class="nav-item">
+						<a href="subsitios/cliente/listado.php" class="nav-link" style="color: black; margin-top: 5px; padding: 0px 10px 0px 10px;">Clientes</a>
+					</li>
+					<li class="nav-item ">
+						<a href="subsitios/presupuesto/listado.php" class="nav-link" style="color: black; margin-top: 5px; border-left: 1px solid black; padding: 0px 10px 0px 10px;">Presupuestos</a>
+					</li>
+					<li class="nav-item ">
+						<a href="subsitios/estadisticas/seleccion_estadisticas.php" class="nav-link" style="color: black; margin-top: 5px; border-left: 1px solid black; padding: 0px 10px 0px 10px;">Estadisticas</a>
+					</li>
+					<li class="nav-item">
+						<a href="objetos/generales/salir.php" class="nav-link" style="color: black; margin-top: 5px; border-left: 1px solid black; padding: 0px 10px 0px 10px;">Salir</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</header>
+</body>
+</html>
