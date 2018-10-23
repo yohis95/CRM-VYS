676ca320a92a2cc5f0eec3fcc6e4ea8a06cedcb0
yohis95 <42844131+yohis95@users.noreply.github.com>
2018-10-23 14:00:47 -0300

    Add files via upload

---
+++ subsitios/estadisticas/procesos/estadistica_sin_filtro.php
@@ -0,0 +1,72 @@
+<?php
+
+
+require('../../../objetos/generales/conexion.php');
+
+
+
+		
+$consulta= "SELECT * FROM tbl_presupuesto";
+$resultado = mysqli_query($connection , $consulta);
+$cantidad_total = mysqli_num_rows($resultado);
+
+if($cantidad_total > 0)
+{
+
+$consulta_cancelado= "SELECT * FROM tbl_presupuesto WHERE idEstado = 1";
+$resultado_cancelado = mysqli_query($connection , $consulta_cancelado);
+$cantidad_cancelado = mysqli_num_rows($resultado_cancelado);
+
+	if($cantidad_cancelado > 0)
+	{
+		$porcentaje_cancelado=(($cantidad_cancelado * 100) / $cantidad_total);
+	}
+	else{
+		$porcentaje_cancelado=0;
+	}
+
+echo "El porcentaje de presupuestos con estado 'Cancelado' es de" . $porcentaje_cancelado ."%";
+
+$consulta_espera= "SELECT * FROM tbl_presupuesto WHERE idEstado = 2 ";
+$resultado_espera = mysqli_query($connection , $consulta_espera);
+$cantidad_espera = mysqli_num_rows($resultado_espera);
+
+	if($cantidad_espera > 0)
+	{
+		$porcentaje_espera=(($cantidad_espera * 100) / $cantidad_total);
+	}
+	else{
+		$porcentaje_espera = 0;
+	}
+
+echo "El porcentaje de presupuestos con estado 'En Espera' es de" . $porcentaje_espera ."%";
+
+$consulta_confirmado= "SELECT * FROM tbl_presupuesto WHERE idEstado = 3 ";
+$resultado_confirmado = mysqli_query($connection , $consulta_confirmado);
+$cantidad_confirmado = mysqli_num_rows($resultado_confirmado);
+
+	if($cantidad_confirmado > 0)
+	{
+		$porcentaje_confirmado=(($cantidad_confirmado * 100) / $cantidad_total);
+	}
+	else{
+		$porcentaje_confirmado = 0;
+	}
+
+	echo "El porcentaje de presupuestos con estado 'Confirmado' es de" . $porcentaje_confirmado ."%";
+}
+
+
+	
+
+	?>
+
+<html>
+<head>
+<script language="JavaScript">
+window.top.location.href="<?=$pagina?>" ;
+
+</script>
+</head>
+
+</html>
