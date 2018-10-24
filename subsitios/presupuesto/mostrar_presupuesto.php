5b7d2efce78a7ce95ca387d1d552bf2629302b6e
yohis95 <42844131+yohis95@users.noreply.github.com>
2018-10-23 21:54:23 -0300

    Update mostrar_presupuesto.php

--- subsitios/presupuesto/mostrar_presupuesto.php
+++ subsitios/presupuesto/mostrar_presupuesto.php
@@ -3,7 +3,7 @@ session_start();
 require('../../objetos/generales/conexion.php');
 $link_error="../../index.php?error_usuario=si";
 require_once('../../objetos/generales/validar.php'); 
-define('MYSQL_ASSOC',MYSQLI_ASSOC);
+
 $id_presupuesto = $_GET["id_presupuesto"];
 $emisor = $_GET["emisor"];/*listado - nuevo_presupuesto*/
 require_once('../../objetos/generales/obtener_variables_get.php'); 
