592848731257d6f621c0ea89e14e3a3f9c780fce
yohis95 <42844131+yohis95@users.noreply.github.com>
2018-10-23 21:48:45 -0300

    Add files via upload

---
+++ subsitios/cliente/listado.php
@@ -0,0 +1,264 @@
+<?php
+session_start();
+require('../../objetos/generales/conexion.php');
+$link_error="../../index.php?error_usuario=si";
+require_once('../../objetos/generales/validar.php'); 
+define('MYSQL_ASSOC',MYSQLI_ASSOC);
+$resultado = "";
+$resultado_accion = "";
+
+foreach ($_GET as $key => $value ) {
+   if (isset($_GET[$key]))
+   	$$key = $value;
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CRM VYS</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" type="image/png" href="../../favicon.ico"/>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<script type="">
+
+</script>
+
+<style type="">
+	
+	.contenedor_formulario {
+ 
+  padding: 20px;
+  margin-left:150px;
+  margin: 20px;
+  background: #fff;
+  border-radius: 10px;
+  overflow: hidden;
+}
+
+.titulo_formulario {
+  display: block;
+  font-weight: 600;
+  padding-bottom:20px;
+
+  font-size: 30px;
+  color: #EE8A12;
+  line-height: 1.2;
+
+}
+
+.logo{
+ 
+  width: 30%;
+  align-content: left;
+  }
+
+
+body{
+	background: url(../../images/fondo.jpg);
+}
+
+header {
+  background: rgba(0,0,0,0.9);
+  width: 100%;
+  position: fixed;
+  z-index: 100;
+  margin-top:-25px;
+
+}
+
+.contenedor_formulario{
+
+margin-top: 70px;
+
+	}
+
+.btn-primary{
+	background: #EE8A12 !important;
+	 border:1px solid #EE8A12 !important;
+}
+
+.contenedor_nuevo {
+
+	border:1px solid #F1EEF0 ;
+	background: #F1EEF0 ;
+	padding: 5px;
+	padding-right: 20px;
+	text-align: right;
+	font-size: 15px;
+	margin-bottom: 15px;
+	border-radius: 14px;
+	color: black;
+		font-weight:500;
+
+
+	}
+
+a:hover .contenedor_nuevo{
+	text-decoration: none;
+	font-weight:600;	
+	border:1px solid #F1EEF0 ;
+	background: #F1EEF0 ;
+	padding: 5px;
+	padding-right: 20px;
+	text-align: right;
+	font-size: 15px;
+	margin-bottom: 15px;
+	border-radius: 14px;
+	color: black;
+	}
+
+table{
+	border-radius:14px;
+
+	background: #F1EEF0 ;
+	
+}
+
+td{
+	width: 300px;
+	padding: 10px;
+	color: black;
+
+}
+
+tr:hover{
+	background: #E0E2E8;
+
+}
+
+</style>
+
+
+
+</head>
+<body>
+
+	<header>
+		<nav class="navbar navbar-expand-lg navbar-dark " style="background: white; border-bottom: 1px solid #D01262; margin-top:-50px; ">
+			<a href="../../principal.php" class="navbar-brand"><img src="../../images/logo.png" width="50"></a>
+			<button class="navbar-toggler" data-target="#navigation" data-control="navigation" data-toggle="collapse">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navigation">
+				<ul class="nav navbar-nav">
+
+					<li class="nav-item">
+						<a href="../../subsitios/cliente/listado.php" class="nav-link" style="color: black; margin-top: 5px; padding: 0px 10px 0px 10px;">Clientes</a>
+					</li>
+					<li class="nav-item ">
+						<a href="../../subsitios/presupuesto/listado.php" class="nav-link" style="color: black; margin-top: 5px; border-left: 1px solid black; padding: 0px 10px 0px 10px;">Presupuestos</a>
+					</li>
+					<li class="nav-item ">
+						<a href="../../subsitios/estadisticas/seleccion_estadisticas.php" class="nav-link" style="color: black; margin-top: 5px; border-left: 1px solid black; padding: 0px 10px 0px 10px;">Estadisticas</a>
+					</li>
+					<li class="nav-item">
+						<a href="../../objetos/generales/salir.php" class="nav-link" style="color: black; margin-top: 5px; border-left: 1px solid black; padding: 0px 10px 0px 10px;">Salir</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</header>
+
+
+
+
+	
+		<div class="container">
+			<div class="contenedor_formulario">
+				<span class="titulo_formulario">
+					Listado de Clientes
+				</span>
+
+
+			<?php	if  ($resultado == "fracaso") {
+				?>
+					 <div class="alert alert-dismissible alert-danger">
+				  		<strong>Ooops!</strong> El cliente no se cargo correctamente. Vuelva a intentarlo.
+					 </div>
+
+				<?php 
+
+			}if($resultado == "exito"){
+				?>
+
+					  <div class="alert alert-dismissible alert-success">
+				  		<strong></strong> El Cliente se cargo correctamente.
+					 </div>
+
+
+				<?php
+				}  
+				?>
+
+				<?php	if  ($resultado_accion == "fracaso") {
+				?>
+					 <div class="alert alert-dismissible alert-danger">
+				  		<strong>Ooops!</strong> No se pudo realizar su petición. Vuelva a intentarlo.
+					 </div>
+
+				<?php 
+
+			}if($resultado_accion == "exito"){
+				?>
+
+					  <div class="alert alert-dismissible alert-success">
+				  		<strong></strong> Su petición se relizo correctamente.
+					 </div>
+
+
+				<?php
+				}  
+				?>
+
+
+				<a href="nuevo_cliente.php"> <div class="contenedor_nuevo">
+					Nuevo Cliente
+				</div></a>
+
+				<table >
+					
+					<?php
+
+					$consulta_clientes="SELECT * FROM tbl_cliente ORDER BY idCliente DESC";
+					$resultado_clientes = mysqli_query($connection , $consulta_clientes);
+					while($rs_clientes = mysqli_fetch_array($resultado_clientes, MYSQL_ASSOC)){
+
+						$id_cliente=$rs_clientes['idCliente'];
+						$nombre=$rs_clientes['nombre'];
+						$apellido=$rs_clientes['apellido'];
+						$dni=$rs_clientes['dni'];
+
+						
+
+					?>
+					
+
+						<tr>
+							<td><?=$nombre?> <?=$apellido?></td>
+							<td>DNI:<?=$dni?> </td>
+							<td><a href="mostrar_cliente.php?id_cliente=<?=$id_cliente?>&emisor=listado" style="color:black; font-size: 13px; ">Mostrar Cliente</a></td>
+							<td><a href="modificar_cliente.php?id_cliente=<?=$id_cliente?>&emisor=listado" style="color:black; font-size: 13px; ">Modificar Cliente</a></td>
+							<td><a href="procesos/eliminar_cliente.php?id_cliente=<?=$id_cliente?>" style="color:black; font-size: 13px; ">Eliminar cliente</a></td>
+
+						</tr>
+
+						<?php 
+
+					}
+						?>
+					
+				</table>
+
+
+				
+			</div>
+		</div>
+		
+</body>
+</html>
---
+++ subsitios/cliente/nuevo_cliente.php
@@ -0,0 +1,297 @@
+<?php
+session_start();
+require('../../objetos/generales/conexion.php');
+$link_error="../../index.php?error_usuario=si";
+require_once('../../objetos/generales/validar.php'); 
+
+define('MYSQL_ASSOC',MYSQLI_ASSOC);
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CRM VYS</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" type="image/png" href="../../favicon.ico"/>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<script type="text/javascript">
+function cancelar() {
+	document.formulario.action = "";
+	document.formulario.submit();
+}
+
+
+function agregar() {		
+
+			if(validar_form(document.formulario) == true)
+			{
+			document.formulario.action = "procesos/agregar_cliente.php";
+			document.formulario.submit();
+			}
+
+	
+}
+
+
+
+function validar_form(theForm) {
+	
+
+	if (theForm.nombre.value == ""){
+		alert("El nombre es un dato requerido.");
+		theForm.nombre.focus();
+		return (false);
+	}
+	
+	if (theForm.apellido.value == ""){
+		alert("El apellido es un dato requerido.");
+		theForm.apellido.focus();
+		return (false);
+	}
+
+	if (theForm.dni.value == ""){
+		alert("El dni es un dato requerido.");
+		theForm.dni.focus();
+		return (false);
+	}
+
+	if (theForm.domicilio.value == ""){
+		alert("El domicilio es un dato requerido.");
+		theForm.domicilio.focus();
+		return (false);
+	}
+	
+	if (theForm.email.value == ""){
+		alert("El email es un dato requerido.");
+		theForm.email.focus();
+		return (false);
+	}
+
+	email = theForm.email.value;
+		
+	if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
+		
+	}
+	else{
+		alert("La dirección de email es incorrecta.");
+		return (false);
+	}	
+
+	if (theForm.telefono.value == ""){
+		alert("El telefono es un dato requerido.");
+		theForm.telefono.focus();
+		return (false);
+	}
+
+	if (theForm.localidad.value == ""){
+		alert("La localidad es un dato requerido.");
+		theForm.localidad.focus();
+		return (false);
+	}
+
+	if (theForm.provincia.value == ""){
+		alert("La provincia es un dato requerido.");
+		theForm.provincia.focus();
+		return (false);
+	}
+
+
+	
+	
+		
+	return (true);
+}
+
+$(function() {
+		  $('#form').on('submit', function (event) {
+   	          	event.preventDefault();
+		  });
+		});
+
+</script>
+
+
+<style type="">
+	
+	.contenedor_formulario {
+ 
+  padding: 20px;
+  margin-left:150px;
+  margin: 20px;
+  background: #fff;
+  border-radius: 10px;
+  overflow: hidden;
+}
+
+.titulo_formulario {
+  display: block;
+  font-weight: 600;
+  padding-bottom:20px;
+
+  font-size: 30px;
+  color: #EE8A12;
+  line-height: 1.2;
+
+}
+
+.logo{
+ 
+  width: 30%;
+  align-content: left;
+  }
+
+body{
+	background: url(../../images/fondo.jpg);
+}
+	
+header {
+  background: rgba(0,0,0,0.9);
+  width: 100%;
+  position: fixed;
+  z-index: 100;
+  margin-top:-25px;
+}
+
+
+
+.contenedor_formulario{
+
+margin-top: 70px;
+
+	}
+
+.btn-primary{
+	background: #EE8A12 !important;
+	 border:1px solid #EE8A12 !important;
+}
+	</style>
+}
+</head>
+<body>
+
+	<header>
+		<nav class="navbar navbar-expand-lg navbar-dark " style="background: white; border-bottom: 1px solid #D01262; ">
+			<a href="../../principal.php" class="navbar-brand"><img src="../../images/logo.png" width="50"></a>
+			<button class="navbar-toggler" data-target="#navigation" data-control="navigation" data-toggle="collapse">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navigation">
+				<ul class="nav navbar-nav">
+
+					<li class="nav-item">
+						<a href="../../subsitios/cliente/listado.php" class="nav-link" style="color: black; margin-top: 5px; padding: 0px 10px 0px 10px;">Clientes</a>
+					</li>
+					<li class="nav-item ">
+						<a href="../../subsitios/presupuesto/listado.php" class="nav-link" style="color: black; margin-top: 5px; border-left: 1px solid black; padding: 0px 10px 0px 10px;">Presupuestos</a>
+					</li>
+					<li class="nav-item ">
+						<a href="../../subsitios/estadisticas/seleccion_estadisticas.php" class="nav-link" style="color: black; margin-top: 5px; border-left: 1px solid black; padding: 0px 10px 0px 10px;">Estadisticas</a>
+					</li>
+					<li class="nav-item">
+						<a href="../../objetos/generales/salir.php" class="nav-link" style="color: black; margin-top: 5px; border-left: 1px solid black; padding: 0px 10px 0px 10px;">Salir</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</header>
+
+
+
+	
+		<div class="container">
+			<div class="contenedor_formulario">
+				<!--Action y method lo agregue para enlazarlo al archivo de php-->
+				<form name="formulario" method="post" id="form">
+					<span class="titulo_formulario">
+				Nuevo Cliente
+					</span>
+
+						
+
+						<div class="form-group">
+ 										
+						
+			<div class="form-group col-md-6"> 
+				<label for="nombre">Nombre</label> <br>
+    			<input type="text" class="form-control" name="nombre" id="nombre" placeholder="Ingrese nombre del cliente">
+    		</div>
+
+    		<div class="form-group col-md-6"> <br>
+    			<label for="apellido">Apellido</label><br>
+    			<input type="text" class="form-control" name="apellido" id="apellido" placeholder="Ingrese apellido del cliente" >
+    		</div>
+
+    		<div class="form-group col-md-6"> <br>
+    			<label for="dni">Dni</label><br>
+    			<input type="text" class="form-control" name="dni" id="dni" placeholder="Ingrese dni del cliente" >
+			</div>
+
+			<div class="form-group col-md-6"> <br>
+    			<label for="domicilio">Domicilio</label> <br>
+    			<input type="text" class="form-control" name="domicilio" id="domicilio" placeholder="Ingrese domicilio del cliente" >
+			</div>
+
+			<div class="form-group col-md-6"> <br>
+    			<label for="email">Email</label> <br>
+    			<input type="email" class="form-control" name="email" id="email" placeholder="Ingrese email del cliente" >
+			</div>
+
+			<div class="form-group col-md-6"> <br>
+    			<label for="telefono">Telefono</label> <br>
+    			<input type="text" class="form-control" name="telefono" id="telefono" placeholder="Ingrese telefono del cliente">
+			</div>
+
+			<div class="form-group col-md-6"> <br>
+    			<label for="localidad">Localidad</label> <br>
+    			<input type="text" class="form-control" name="localidad" id="localidad" placeholder="Ingrese localidad">
+			</div>
+
+    		<div class="form-group col-md-6"> <br>
+    			<label for="provincia">Provincia</label> <br>
+    			<select class="form-control" id="provincia" name="provincia">
+						<option name="provincia" id="provincia" value="">Selecciona una provincia</option>
+    			<?php
+    			$consulta_provincias = "SELECT * FROM tbl_provincias";
+    			$resultado_provincias = mysqli_query($connection , $consulta_provincias);
+				while($rs_provincias = mysqli_fetch_array($resultado_provincias, MYSQL_ASSOC)){
+					$id_provincia=$rs_provincias['idProvincia'];
+    			?>
+    			<option name="provincia" id="provincia" value="<?=$id_provincia?>"><?=$rs_provincias['provincia']?> </option>
+    			<?php
+
+}	   										
+    			?>
+    				
+    		</select>
+    			
+    		</div>
+
+
+    	<!--	<div class="form-group ">
+ 										<label for="email">Nombre y apellido</label>
+ 									   <input type="text" name="nombre" class="form-control" id="nombre" placeholder="Ingrese aquí su nombre">
+
+						</div>-->
+
+
+					<div class="form-group "> <br>
+		                <input type="submit" class="btn btn-primary" value="Volver al listado" onClick="cancelar()">
+		                <input type="submit" class="btn btn-primary"   value="Guardar cliente" onClick="agregar()" >
+		            </div>    
+
+				
+
+						
+				</form>
+			</div>
+		</div>
+		
+
+	
+
+</body>
+</html>
